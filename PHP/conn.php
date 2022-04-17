0fbf7d782c552aef01df1801911a7a841acf0349
kxijxn <86701905+kxijxn@users.noreply.github.com>
2022-04-17 14:06:08 +0800

    Delete PHP directory

--- PHP/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","valleyclinic");
-
-//Check connection
-if (mysqli_connect_error())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
